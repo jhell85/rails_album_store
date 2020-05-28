fa6e66fea790df5db555e3d8dd64d2816a6bd387
jhell85 <jhell85@gmail.com>
2020-05-28 09:21:07 -0700

    add migration for add_artists_and_join_table.rb

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -13,6 +13,7 @@ class AlbumsController < ApplicationController
   def create 
     @album = Album.new(album_params)
     if @album.save
+      flash[:notice] = "Album successfully added!"
       redirect_to albums_path
     else
       render :new
@@ -32,6 +33,7 @@ class AlbumsController < ApplicationController
   def update 
     @album = Album.find(params[:id])
     if @album.update(album_params)
+      flash[:notice] = "Album successfully updated!"
       redirect_to albums_path
     else
       render :edit
@@ -41,6 +43,7 @@ class AlbumsController < ApplicationController
   def destroy 
     @album = Album.find(params[:id])
     @album.destroy
+    flash[:notice] = "Album successfully deleted!"
     redirect_to albums_path
   end
 
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,12 @@
 class Album < ApplicationRecord
   has_many :songs, dependent: :destroy
+  validates :name, presence: true
+  validates_length_of :name, maximum: 100
+  has_and_belongs_to_many(:artists)
+  before_save(:titleize_album)
+
+  private
+  def titleize_album
+    self.name = self.name.titleize
+  end
 end
